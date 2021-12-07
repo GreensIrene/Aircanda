52124c80638b83178f8c4509a8d628024b1531c0
GreensIrene <albeenairene28@gmail.com>
2021-12-07 10:07:47 +0530

    Password Functionality completed

--- src/org/Login/Login.java
+++ src/org/Login/Login.java
@@ -2,4 +2,11 @@ package org.Login;
 
 public class Login {
 
+	public static void passWord() {
+		System.out.println("password Functionality");
+	}
+	
+	public static void main(String[] args) {
+		passWord();
+	}
 }
